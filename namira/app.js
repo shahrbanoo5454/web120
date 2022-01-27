85aca682ae592138f543b4b187261f40485a0a07
shahrbanoo5454 <87280526+shahrbanoo5454@users.noreply.github.com>
2022-01-27 14:00:59 +0330

    add

--- namira/app.js
+++ namira/app.js
@@ -1,18 +1,26 @@
-// var $price=document.getElementById('price').value;
-// var $prepayment=document.getElementById('prepayment').innerHTML;
-// var $insrallments=document.getElementById('insrallments').innerHTML;
+
 
 function aqsat(){
-    var $price=document.getElementById('price').value;
-    $price=Number($price);
-// var $prepayment=document.getElementById('prepayment').innerHTML;
-// var $insrallments=document.getElementById('insrallments').innerHTML;
 
-    if($price<=50000000 && $price>=20000000){
-        var hesab=$price*20/10;
-        document.getElementById('prepayment').value=hesab;
+    var $pricee=document.getElementById('pricee').value;
+   
+    
+   var hesab=0;
+    hesab+=(20*$pricee)/100;
+
+    if($pricee<=50000000 && $pricee>=20000000){
+       
+       
+    
+       document.getElementById('prepayment').value=hesab;
+    console.log(hesab);
+            }
+    else{
+        alert("hh");
     }
-    return;
+    
+   
+
 }
 
 
